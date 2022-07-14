2ccc88599e8152d7725f1e44a65b39146f3ba94c
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-07-14 18:45:37 -0400

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
