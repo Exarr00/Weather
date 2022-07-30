d1981ada3ba8e1b1a9394898e43f6b62c9d78b9d
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-07-30 15:31:27 -0400

    error msg

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,34 @@
-import {updatePage, changeDegree} from "./update.js";
+import { updatePage, changeDegree } from './update.js';
 
-const wname = document.querySelector('.city')
-const searchForm = document.getElementById('search_form')
-const city = document.querySelector('input[name=city]')
-const degree = document.getElementById('degrees')
+const error_msg = document.querySelector('.error-msg');
+const searchForm = document.getElementById('search-form');
+const city = document.querySelector('input[name=city]');
+const degree = document.getElementById('degrees');
 
 function fetchWeather(e) {
-    e.preventDefault();
-    fetch(`/search/${city.value}`).then((res) => {
-        if(res.ok){
-            return res.json()
-        }
-        throw new Error('A problem occured while searching')
-    }).then((data) => {
-        updatePage(data);
-        city.value = "";
-    }).catch(error => {console.log(error)})
+  e.preventDefault();
+  fetch(`/search/${city.value}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('A problem occured while searching');
+    })
+    .then((data) => {
+      updatePage(data);
+      city.value = '';
+    })
+    .catch((error) => {
+      console.log(error);
+      error_msg.style.display = 'inline';
+      city.value = '';
+      setTimeout(() => {
+        error_msg.style.removeProperty('display');
+      }, 3000);
+    });
 }
 
-searchForm.addEventListener('submit', fetchWeather)
-degree.addEventListener('click', changeDegree)
+searchForm.addEventListener('submit', fetchWeather);
+degree.addEventListener('click', changeDegree);
 
 //minamilist wallpaper desktop landscape
